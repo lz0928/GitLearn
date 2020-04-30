64024c5109f191d9f9919278ef5fb53f43e21561
Louis Cai <louis.cai@picovr.com>
2020-04-30 17:01:48 +0800

    dev add dog

---
+++ app/src/main/java/com/louis/gitlearn/Dog.java
@@ -0,0 +1,42 @@
+package com.louis.gitlearn;
+
+public class Dog {
+    private String name;
+    private int age;
+    private String color;
+
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
