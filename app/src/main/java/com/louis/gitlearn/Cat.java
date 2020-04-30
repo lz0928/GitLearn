6d92e82f49afb438ca994aa8f81648f5c72a512f
Louis Cai <louis.cai@picovr.com>
2020-04-30 16:48:08 +0800

    Add cat color

--- app/src/main/java/com/louis/gitlearn/Cat.java
+++ app/src/main/java/com/louis/gitlearn/Cat.java
@@ -3,10 +3,21 @@ package com.louis.gitlearn;
 public class Cat {
     private String name;
     private int age;
+    private String color;
+
+    public Cat(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
 
     public Cat(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public void setName(String name) {
@@ -23,5 +34,9 @@ public class Cat {
 
     public int getAge() {
         return age;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
